a81bc80d2e65c35e84366d17fa708dec5777e201
marjan <marjan@tornado.(none)>
2013-05-28 13:22:52 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+        <link href="css/normalize.css" rel="stylesheet" />
+        <link href="css/style.css" rel="stylesheet">
+        <script  src="js/jquery-1.10.0.js"></script>
+        <script  src="js/custom.js"></script>
+    </head>
+    <body>
+        <header>
+            <ul>
+                <li><a href="#" class="page-size-full" data-viewport="100%x100%" data="Full screen"><i class="icon icon-full-size-white icon-transparent"></i></a></li>
+                <li><a href="#" class="page-size" data-viewport="320x480" data="Apple iPhone"><i class="icon icon-phone-portrait-s-white icon-transparent"></i></a></li>
+                <li><a href="#" class="page-size" data-viewport="320x568" data="Apple iPhone 5"><i class="icon icon-phone-portrait-white icon-transparent"></i></a></li>
+                <li><a href="#" class="page-size" data-viewport="600x800" data="Small tablet"><i class="icon icon-tablet-portrait-s-white icon-transparent"></i></a></li>
+                <li><a href="#" class="page-size" data-viewport="768x1024" data="Tablet"><i class="icon icon-tablet-portrait-white icon-transparent"></i></a></li>
+                <li><a href="#" class="page-size" data-viewport="1280x800" data="Widescreen"><i class="icon icon-widescreen-white icon-transparent"></i></a></li>
+                <li><a href="#" class="custom-page-size" data-viewport="1280x800" data="Custom"><i class="icon icon-plus-white icon-transparent"></i></a></li>
+            </ul>
+
+            <p class="icon-description">Apple iPhone "320x480"</p>
+            <button class="add_http http_active" value="http://">http://</button>
+            <input type="text" class="page-link" value="" placeholder="Insert page link..."/>
+            <button class="load-page btn" value="Load Page">Load page</button>
+        </header>
+        <div class="custom-size-container">
+              <input type="text" class="custom-width" value="" /><span>x</span>
+              <input type="text" class="custom-height" value="" />
+              <button class="custom-btn"><i class="icon-crop-white icon-transparent"></i></button>
+        </div>
+        <div class="container">
+            <div class="page-container">
+                <iframe class="page" src=""></iframe>
+                <!--<iframe class="page" src="http://localhost/testApp/responseTable"></iframe>-->
+            </div>
+        </div>
+
+    </body>
+</html>
---
+++ js/custom.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $('.page-size').on('click', function(){
+        var size = $(this).attr('data-viewport');
+        changeSize(size, '.page-container');
+    });
+    $('.page-size-full').on('click', function(){
+        $('.page-container').css('width', '100%');
+        $('.page-container').css('height', '100%');
+    });
+
+    $('.custom-page-size').on('click', function(){
+         $('.custom-size-container').toggleClass('visible-container');
+    });
+
+    $('.icon').on('mouseover', function(){
+        var device = $(this).parent().first().attr('data');
+        var size = $(this).parent().first().attr('data-viewport');
+        $('.icon-description').text( device + ' "' + size + '"' );
+        $('.icon-description').text( device + " '" + size + "'" );
+    });
+
+    $('.load-page').on('click', function(){
+        var pl = $('.page-link').val();
+        var http = $('.add_http');
+
+        if ( http.hasClass( 'http_active' ) ){
+            $('.page').attr('src',( 'http://' + pl));
+        }else{
+            $('.page').attr('src', pl);
+        }
+    });
+
+    $('.add_http').on('click', function(){
+        $(this).toggleClass( 'http_active' );
+    });
+
+
+    $('.custom-btn').on('click', function(){
+        var size = $(this).attr('data-viewport');
+        var w = $('.custom-width').val();
+        var h = $('.custom-height').val();
+
+        $('.page-container').css('width', w);
+        $('.page-container').css('height', h);
+        var container_size = (parseInt(h)+50).toString();
+        $('.container').css('height', container_size);
+    });
+
+    $('.page-link').keypress(function(e) {
+        if (e.which == 13) {
+            var pl = $(this).val();
+            var http = $('.add_http');
+
+            if ( http.hasClass( 'http_active' ) ){
+                $('.page').attr('src',( 'http://' + pl));
+            }else{
+                $('.page').attr('src', pl);
+            }
+        }
+    });
+});
+
+function changeSize(size, elem){
+    var s = size.split('x');
+    $(elem).css('width', s[0]);
+    $(elem).css('height', s[1]);
+    var container_size = (parseInt(s[1])+50).toString();
+    $('.container').css('height', container_size);
+
+}
